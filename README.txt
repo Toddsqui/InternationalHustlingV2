679950cd40a1bf927fadd991add47bf23133c049
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2016-05-14 10:34:49 +0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is for my blog, International Hustling. v. 2.
+
+A Pen created at CodePen.io. You can find this one at http://codepen.io/perry_nt/pen/XXLrNY.
+
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).on("scroll",function(){
+  if($(document).scrollTop()>50){
+    $("nav").removeClass("large").addClass("small");
+  } else{
+    $("nav").removeClass("small").addClass("large");
+  }
+});
